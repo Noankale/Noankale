8de070734cd50e4ba3466c3bcf040d8fa6626502
Noankale <844974473@qq.com>
2025-10-15 21:02:01 +0800

    大概创一下结构

---
+++ arithmetic/generator.py
@@ -0,0 +1,9 @@
+# 题目相关处理模块
+
+# 生成题目与答案
+class ExerciseGenerator:
+  print(111)
+
+# 批改题目
+class ExerciseChecker:
+  print(111)
---
+++ arithmetic/main.py
@@ -0,0 +1,12 @@
+# 函数使用_，变量使用小驼峰
+
+def write_to_file(filename,content):
+  return
+
+def main():
+  # 检查用户命令行输入
+  # 若为生成模式 -> 调用生成题目函数
+  # 将题目和答案分别导出
+  # 若为批改模式 -> 调用批改函数
+  # 导出批改结果
+  return
